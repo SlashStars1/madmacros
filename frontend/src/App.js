f6be017f42012765ca26f63174eae7207d5b00e2
Suha Afzar <suha.afzar@gmail.com>
2024-05-28 11:22:19 -0500

    Added changes to css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,7 @@
 
 import "./App.css";
-import Form from './components/Form';
-import Menu from './components/Menu';
 import NavBar from './components/NavBar';
 import React, {useState, useEffect} from "react";
-import Header from "./components/Header/Header";
 import {AuthContextProvider} from './components/store/auth-context';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from "./components/Home";
