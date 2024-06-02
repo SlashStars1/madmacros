bc4473fe6cbd64bf387ae85ece4873f2a7312543
Suha Afzar <suha.afzar@gmail.com>
2024-06-02 15:22:24 -0500

    Added restaraunt headers to favorites page

--- frontend/src/components/FavoritesTwo.js
+++ frontend/src/components/FavoritesTwo.js
@@ -40,6 +40,22 @@ const {user} = useUserAuthContext()
         }
     }
 
+    let Jamba = {};
+    let Chipotle = {};
+    let Chic_Fil_A = {};
+    let Ihop = {};
+    let Shake_Shack={};
+    let Panera_Bread = {};
+    if (favs.length>0){
+      Chic_Fil_A = favs.filter((fav)=>fav.food==="Chic Fil A")
+      Chipotle = favs.filter((fav)=>fav.food==="Chipotle")
+Ihop = favs.filter((fav)=>fav.food=="Ihop");
+Jamba = favs.filter((fav)=> fav.food=="Jamba");
+Shake_Shack = favs.filter((fav)=> fav.food=="Shake Shack")
+
+Panera_Bread = favs.filter((fav)=> fav.food==="Panera Bread")
+    }
+
 
     
 
@@ -56,7 +72,11 @@ Please login to see your favorite meals!</p></div>
     <div className="no-favorites"> <img src={Heart}/><p>
         You have no favorites. Heart a meal to see them here!</p></div>: <></>}
 
-        {(favs.length > 0)? <> {favs.map(fav => (
+        {(Jamba.length > 0)?  
+        (<> 
+        <h2>Jamba</h2>
+        {
+        Jamba.map(fav => (
             <FavMeal
               key={fav._id}
               id={fav._id}
@@ -67,7 +87,98 @@ Please login to see your favorite meals!</p></div>
               serving={fav.serving}
               fav={true}
             />
-          ))}</> :  <></> }
+          ))}</>):  <></> }
+
+          
+        {(Chipotle.length > 0)?  
+        (<> 
+        <h2>Chipotle</h2>
+        {
+        Chipotle.map(fav => (
+            <FavMeal
+              key={fav._id}
+              id={fav._id}
+              name={fav.name}
+              food={fav.food}
+              protein={fav.protein}
+              calories={fav.calories}
+              serving={fav.serving}
+              fav={true}
+            />
+          ))}</>):  <></> }
+
+
+{(Chic_Fil_A.length > 0)?  
+        (<> 
+        <h2>Chic Fil A</h2>
+        {
+        Chic_Fil_A.map(fav => (
+            <FavMeal
+              key={fav._id}
+              id={fav._id}
+              name={fav.name}
+              food={fav.food}
+              protein={fav.protein}
+              calories={fav.calories}
+              serving={fav.serving}
+              fav={true}
+            />
+          ))}</>):  <></> }
+
+
+{(Panera_Bread.length > 0)?  
+        (<> 
+        <h2>Panera Bread</h2>
+        {
+        Panera_Bread.map(fav => (
+            <FavMeal
+              key={fav._id}
+              id={fav._id}
+              name={fav.name}
+              food={fav.food}
+              protein={fav.protein}
+              calories={fav.calories}
+              serving={fav.serving}
+              fav={true}
+            />
+          ))}</>):  <></> }
+
+
+       
+{(Ihop.length > 0)?  
+        (<> 
+        <h2>Ihop</h2>
+        {
+        Ihop.map(fav => (
+            <FavMeal
+              key={fav._id}
+              id={fav._id}
+              name={fav.name}
+              food={fav.food}
+              protein={fav.protein}
+              calories={fav.calories}
+              serving={fav.serving}
+              fav={true}
+            />
+          ))}</>):  <></> }
+
+{(Shake_Shack.length > 0)?  
+        (<> 
+        <h2>Shake Shack</h2>
+        {
+        Shake_Shack.map(fav => (
+            <FavMeal
+              key={fav._id}
+              id={fav._id}
+              name={fav.name}
+              food={fav.food}
+              protein={fav.protein}
+              calories={fav.calories}
+              serving={fav.serving}
+              fav={true}
+            />
+          ))}</>):  <></> }
+
        
        
        </div>
