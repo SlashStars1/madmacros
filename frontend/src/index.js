2a4f9cfdf3e986e8e6b0338db989352d82f5e322
Suha Afzar <suha.afzar@gmail.com>
2024-06-04 14:36:37 -0500

    Added vercel analytics

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     
     <App />
-    
+    <Analytics />
+
   </React.StrictMode>
 );
 
